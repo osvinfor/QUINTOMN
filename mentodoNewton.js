4159c3d43ef8f6a0ce9e08d32f342384ed263bfa
Oswaldo Mendoza <oswaldo.1979@icloud.com>
2023-06-14 17:54:25 -0500

    Tiempo4

--- mentodoNewton.js
+++ mentodoNewton.js
@@ -1,18 +1,26 @@
 function metodoNewton(){
+	alert("Hola newtom");
+
+}
+//Metodo de la biseccion
+function bisexccion(){
 // Obtener los valores de los puntos ingresados
 var a = parseInt(document.getElementById("x1").value);
 var b = parseInt(document.getElementById("y1").value);
 var i = parseInt(document.getElementById("x2").value);
 for(var cont=1; cont<=i ;cont++){
 	m=(a+b)/2
+
 	var fa=(a**a)-100;
 	var fb=(b**b)-100;
 	var fm=(m**m)-100;
 
+
 	if ((fa*fm)<0) {b=m;}
 	if ((fm*fb)<0) {a=m;}
-	document.write("El nuenvo intervalo es para a: "+a+" y para b: "+b);
+
 	//alert("El nuenvo intervalo es para a: "+a+" y para b: "+b);
+	document.write("El nuenvo intervalo es para a: "+a+" y para b: "+b);
 }
 
 }
